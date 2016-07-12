8ab7dac0bf7b282ecd4572ddd4b2dd736698d556
qiansy <chinaqstar@gmail.com>
2016-07-12 13:36:39 +0800

    动态menu

--- src/views/app.js
+++ src/views/app.js
@@ -1,12 +1,37 @@
+// template 用来替换html中的<div id="example" />
+var appHtml = '<div class="example-nav"><nav><div class="nav-title"><h2><a href="#">Vue Demo</a></h2></div><ul class="nav-list"><li><a @click.prevent="listClick(\'home\', $event)">home</a></li><li><a @click.prevent="listClick(\'hello\', $event)">hello</a></li></ul></nav></div><div class="example-container"><component :is="currentView"></component></div>';
+
+// vue 渲染模板，通过
+// <component :is="currentView"> 来动态显示view模块
 var App = Vue.extend({
-  template: '<hello></hello>',
+  template: appHtml,
+  data() {
+    return {
+      currentView: 'home'
+    }
+  },
   components: {
-    'hello': {
-      template: '<div>hello world.</div>'
+    home: {
+      template: '<div>this is home</div>'
+    },
+    hello: {
+      template: '<div>hello</div>'
+    }
+  },
+  methods: {
+    listClick(code, e) {
+      if(code == 'home') {
+        this.currentView = 'home';
+      } else if(code == 'hello') {
+        console.log('hello');
+        this.currentView = 'hello';
+      }
     }
   }
 })
 
-new App({
+// 挂载dom元素，相当于：
+// new App().$mount("#example");
+var app = new App({
   el: '#example'
 })
